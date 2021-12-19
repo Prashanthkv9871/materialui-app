dad27cc50020e77018a59a363dad61ec5c7369a2
Prashanthkv9871 <kvprashanth987@gmail.com>
2021-12-19 23:14:42 +0530

    materialui-app

---
+++ src/components/Leftbar.jsx
@@ -0,0 +1,11 @@
+import {makeStyles} from '@material-ui/core'
+
+
+const useStyles = makeStyles(()=>({}));
+
+const Leftbar=()=> {
+    const classes = useStyles();
+    return <div></div>
+}
+
+export default Leftbar;
